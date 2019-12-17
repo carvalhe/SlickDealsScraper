98ed51c579449455e26c0a26030fd1126261181f
Dev Mac <devmac@Devs-MacBook-Pro.local>
2019-12-17 14:41:00 -0800

    Can find name of item and check keyword

--- SlickDealsScraper.py
+++ SlickDealsScraper.py
@@ -2,11 +2,12 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import re
 
 DEBUG = False
 
 #### Set up where you are requesting from
-keyWords = "laptop"
+keyWords = "Switch"
 # Add the user_input toe the google search url and request that
 domain = 'https://slickdeals.net/'
 source = requests.get(domain).text
@@ -27,6 +28,13 @@ if(source is not None):
     # need the Div with a class of fpitem
     #for item in soup.find_all('div', class_= "fpitem  pctoff"):
     item = soup.find_all("div", class_= "fpItem")
-    print(item[1])
-
+    print(item[1].prettify())
+    #link = item[1].find("a", class_="itemTitle")
+    # we now have the item title. we need to search it to check if it contains the keywords we are looking for
+    link = item[1].find("a", class_="itemTitle")
+    print(link.text)
+    if("Arts" in link.text):
+        print("yes")
+    else:
+        print("no")
 csv_file.close()
