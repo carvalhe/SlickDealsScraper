1b14ec65deb64e94cf92b7ae60398cbd2e878429
Dev Mac <devmac@Devs-MacBook-Pro.local>
2019-12-19 21:59:10 -0800

    Scans through frontpage for keywords

--- SlickDealsScraper.py
+++ SlickDealsScraper.py
@@ -4,10 +4,10 @@ import requests
 import csv
 import re
 
-DEBUG = True
+DEBUG = False
 
 #### Set up where you are requesting from
-keyWords = ["Switch", "Music"]
+keyWords = ["Switch", "Music", "Dark", "Xbox"]
 # Add the user_input toe the google search url and request that
 domain = 'https://slickdeals.net/'
 source = requests.get(domain).text
@@ -26,27 +26,32 @@ if(source is not None):
     #### Parse through the soup for the information you want
     # need the Div with a class of fpitem
     #for item in soup.find_all('div', class_= "fpitem  pctoff"):
-    item = soup.find_all("div", class_= "fpItem")
-    if(DEBUG):
-        print(item[1].prettify())
-    #link = item[1].find("a", class_="itemTitle")
-    # we now have the item title. we need to search it to check if it contains the keywords we are looking for
-    link = item[1].find("a", class_="itemTitle")
-    if(DEBUG):
-        print(link.text)
-    # we have the link to an item, scan through its conents to see if it matches
-    # with what we are searching for aka keyWords list
-    for key in keyWords:
-        if(key in link.text):
-            if(DEBUG):
-                print("Keyword match")
-            # the key matches, add it to the csv
-            csv_file.write(link.text)
-            # print(item[1].find("a", class_= "viewDetailsBtn"))
-            urlDeal = item[1].find_all('a', href=True)
-            urlDeal = "slickdeals.net" + urlDeal[1]['href']
-            print(urlDeal)
-            # break out so you dont add it twice
-            break
+    #item = soup.find_all("div", class_= "fpItem")
+    for item in soup.find_all("div", class_= "fpItem"):
+        if(DEBUG):
+            print(item.prettify())
+        #link = item[1].find("a", class_="itemTitle")
+        # we now have the item title. we need to search it to check if it contains the keywords we are looking for
+        link = item.find("a", class_="itemTitle")
+        if(link == None):
+            continue
+        if(DEBUG):
+            print(link.text)
+        # we have the link to an item, scan through its conents to see if it matches
+        # with what we are searching for aka keyWords list
+        for key in keyWords:
+            if(key in link.text):
+                if(DEBUG):
+                    print("Keyword match")
+                # the key matches, add it to the csv
+                #csv_file.write(link.text)
+                # print(item[1].find("a", class_= "viewDetailsBtn"))
+                urlDeal = item.find_all('a', href=True)
+                urlDeal = "slickdeals.net" + urlDeal[1]['href']
+                if(DEBUG):
+                    print(urlDeal)
+                csv_writer.writerow([key, urlDeal])
+                # break out so you dont add it twice
+                break
 
 csv_file.close()
